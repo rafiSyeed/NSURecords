d9eda13117ccd3575a60bb702172b07bcde4a602
RafiSyeed <rafi8800@gmail.com>
2016-05-24 19:01:56 +0600

    2nd Commit

--- app/src/main/java/com/example/administrator/nsurecords/MainActivity.java
+++ app/src/main/java/com/example/administrator/nsurecords/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.nsurecords;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,5 +29,9 @@ public class MainActivity extends AppCompatActivity {
         detail.setEmail(email.getText().toString());
         helper.insert(detail);
 
+    }
+    public void display(View view){
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
     }
 }
